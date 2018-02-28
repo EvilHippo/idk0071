ad5db8e76693d53704fa47d2040ab730ec43d3f9
helaur <helaur@ttu.ee>
2018-02-28 22:32:22 +0200

    fixed bug with coordinates and made world generation a bit better

--- springboot/src/main/java/com/spring/world/World.java
+++ springboot/src/main/java/com/spring/world/World.java
@@ -12,8 +12,8 @@ public class World {
     private int height;
     private int numberOfPlatforms;
     public static final int PLATFORM_NUMBER_OF_PIXELS = 10;
-    public static final int MINIMUM_DISTANCE_BETWEEN_PLATFORMS = 2 * PLATFORM_NUMBER_OF_PIXELS;
-    public static final int MAXIMUM_DISTANCE_BETWEEN_PLATFORMS = 2 * PLATFORM_NUMBER_OF_PIXELS;
+    public static final int MINIMUM_DISTANCE_BETWEEN_PLATFORMS = 3;
+    public static final int MAXIMUM_DISTANCE_BETWEEN_PLATFORMS = 5;
     public static final int MAXIMUM_NUMBER_OF_PLATFORM_NEXT_TO_EACH_OTHER = 3;
     private List<Platform> platforms = new ArrayList<>();
 
@@ -54,7 +54,20 @@ public class World {
         this.width = width;
         this.height = height;
         this.numberOfPlatforms = numberOfPlatforms;
-        generateWorld();
+
+        platforms.addAll(generateWorld(new ArrayList<>()));
+        platforms.addAll(generateWorld(new ArrayList<>()));
+        platforms.addAll(generateWorld(new ArrayList<>()));
+        platforms.addAll(generateWorld(new ArrayList<>()));
+        platforms.addAll(generateWorld(new ArrayList<>()));
+        platforms.addAll(generateWorld(new ArrayList<>()));
+        platforms.addAll(generateWorld(new ArrayList<>()));
+        platforms.addAll(generateWorld(new ArrayList<>()));
+        platforms.addAll(generateWorld(new ArrayList<>()));
+        platforms.addAll(generateWorld(new ArrayList<>()));
+        platforms.addAll(generateWorld(new ArrayList<>()));
+        platforms.addAll(generateWorld(new ArrayList<>()));
+        printCoordinates();
     }
     public World(List<Platform> platforms) {
 
@@ -63,30 +76,49 @@ public class World {
 
 
 
-    private void generateWorld() {
+    private List<Platform> generateWorld(List<Platform> platforms) {
         for (int i = 0; i < numberOfPlatforms; i++) {
-            createPlatForm();
+            createPlatForm(platforms);
         }
+        return platforms;
     }
 
-    private void createPlatForm() {
-        int x = new Random().nextInt(width) / PLATFORM_NUMBER_OF_PIXELS;
-        int y = new Random().nextInt(MAXIMUM_DISTANCE_BETWEEN_PLATFORMS) + MINIMUM_DISTANCE_BETWEEN_PLATFORMS;
+    private void createPlatForm(List<Platform> platforms) {
+
+        int y = (new Random().nextInt(MAXIMUM_DISTANCE_BETWEEN_PLATFORMS) + MINIMUM_DISTANCE_BETWEEN_PLATFORMS) * PLATFORM_NUMBER_OF_PIXELS;
+
         int numberOfPlatforms = new Random().nextInt(MAXIMUM_NUMBER_OF_PLATFORM_NEXT_TO_EACH_OTHER) + 2;
+
         if(platforms.isEmpty()) {
-            for (int i = 0; i < numberOfPlatforms; i++) {
-                platforms.add(new BasicPlatform(x + i * PLATFORM_NUMBER_OF_PIXELS, y + i * PLATFORM_NUMBER_OF_PIXELS));
-            }
+            int x = 10 + new Random().nextInt(80) * PLATFORM_NUMBER_OF_PIXELS;
+            y = 10;
+            platforms.add(new BasicPlatform(x , y));
+
+
         } else {
-            double lastPlacedPlatformX = platforms.get(platforms.size() - 1).getX();
+            double lastPlacedPlatformX = platforms.get(platforms.size() - 1).getX() + (Math.pow(-1, new Random().nextInt(5) + 1) * ((new Random().nextInt(10)) * PLATFORM_NUMBER_OF_PIXELS));
+            System.out.println((Math.pow(-100, new Random().nextInt(2) + 1) * ((new Random().nextInt(3) + 3) * PLATFORM_NUMBER_OF_PIXELS)));
             double lastPlacedPlatformY = platforms.get(platforms.size() - 1).getY();
             for (int i = 0; i < numberOfPlatforms; i++) {
-                int totalX = (int)(x + i * PLATFORM_NUMBER_OF_PIXELS + lastPlacedPlatformX);
+                int totalX = (int)(i * PLATFORM_NUMBER_OF_PIXELS + lastPlacedPlatformX);
                 if(totalX > width) {
-                    totalX -= totalX - width;
+                    while (totalX > width) {
+                        totalX -= (new Random().nextInt(2) + 1) * PLATFORM_NUMBER_OF_PIXELS;
+                    }
+                } else if(0 > totalX) {
+                    while (0 > totalX) {
+                        totalX += (new Random().nextInt(2) + 1) * PLATFORM_NUMBER_OF_PIXELS;
+                    }
                 }
-                platforms.add(new BasicPlatform(totalX, y + i * PLATFORM_NUMBER_OF_PIXELS + lastPlacedPlatformY));
+                platforms.add(new BasicPlatform(totalX, y + lastPlacedPlatformY));
             }
+        }
+
+    }
+    private void printCoordinates() {
+        for (Platform platform :
+                platforms) {
+            System.out.println("X: " + platform.getX() + " Y: " + platform.getY());
         }
     }
 }
